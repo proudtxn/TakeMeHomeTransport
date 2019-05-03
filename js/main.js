57c3d708513c1dafffae635c9ff0b66e4fab0407
Phillip Pense <39144472+proudtxn@users.noreply.github.com>
2019-05-03 12:43:36 -0500

    added style to side navbar and js for hiding and displaying it.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function hideNav() {
+    document.getElementById("sideBar").classList.toggle("active");
+    document.getElementById("toggleBtn").classList.toggle("active");
+    document.getElementById("menuBtn").classList.toggle("active");
+    document.getElementById("rightSide").classList.toggle("active");
+}
