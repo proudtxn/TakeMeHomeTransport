3e0f1e4ad659754fa4b93d3c14f3ab11bb207603
Phillip Pense <39144472+proudtxn@users.noreply.github.com>
2019-05-07 19:50:52 -0500

    added FAQ page code and style and added footer.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-function hideNav() {
-    document.getElementById("sideBar").classList.toggle("active");
-    document.getElementById("toggleBtn").classList.toggle("active");
-    document.getElementById("menuBtn").classList.toggle("active");
-    document.getElementById("rightSide").classList.toggle("active");
-    document.getElementById("eventList").classList.toggle("active");
-    document.getElementById("calendar").classList.toggle("active");
+function hideNav () {
+	document.getElementById('sideBar').classList.toggle('active');
+	document.getElementById('toggleBtn').classList.toggle('active');
+	document.getElementById('menuBtn').classList.toggle('active');
+	document.getElementById('rightSide').classList.toggle('active');
+	document.getElementById('eventList').classList.toggle('active');
+	document.getElementById('calendar').classList.toggle('active');
 }
+
+$(document).ready(function () {
+
+	// SMOOTH SCROLLING - jQuery
+	var scrollLink = $('.scroll');
+
+	// Smooth scrolling
+	scrollLink.click(function (e) {
+		e.preventDefault();
+		$('body,html').animate(
+			{
+				scrollTop: $(this.hash).offset().top
+			},
+			1000
+		);
+	});
+});
